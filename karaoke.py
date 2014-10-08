2e42c6aa7aed0bb772a38c397964b6b50e64c6e9
Nath <nath@nath-VirtualBox.(none)>
2014-10-08 21:34:06 +0200

    ejercicio 4

--- karaoke.py
+++ karaoke.py
@@ -2,22 +2,34 @@
 # -*- coding: iso-8859-15 -*-
 
 import sys
+from smallsmilhandler import SmallSMILHandler
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
-if __name__ == "__main__":
-
-    try:
-        fichero = sys.argv[1] 
-    except IndexError:
-        print "Usage: python karaoke.py file.smil"
-        raise SystemExit    
-    
-    try:
-        fich = open(fichero, 'r') 
-        lista_fichero = fich.readlines()
-    except IOError:
-        print "Introduce un nombre correcto de fichero"
-        raise SystemExit  
-        
+try:
+    fichero = sys.argv[1] 
+except IndexError:
+    print "Usage: python karaoke.py file.smil"
+    raise SystemExit    
+try:
+    fich = open(fichero, 'r') 
     fich.close()
+except IOError:
+    print "Introduce un nombre correcto de fichero.smil"
+    raise SystemExit  
+
+parser = make_parser()
+kHandler = SmallSMILHandler()
+parser.setContentHandler(kHandler)
+parser.parse(open(fichero))
     
-    print lista_fichero
+for i in range(len(kHandler.lista)):
+    linea = kHandler.lista[i]
+    tag = linea['name']
+    imprimir = tag
+    for atributo in kHandler.atributos[tag]:
+        if not linea[atributo] == "":
+            imprimir = imprimir + "\\t" + atributo + '="' + linea[atributo] + '"'
+    imprimir = imprimir + "\\n"
+    print imprimir
+

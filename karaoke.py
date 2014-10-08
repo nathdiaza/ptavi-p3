3ad79eafceb8f3a2e242fe46ffc9b439a9300925
Nath <nath@nath-VirtualBox.(none)>
2014-10-08 23:04:50 +0200

    ejercicio 5

--- karaoke.py
+++ karaoke.py
@@ -5,31 +5,38 @@ import sys
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import os
 
 try:
-    fichero = sys.argv[1] 
+    fichero = sys.argv[1]
 except IndexError:
     print "Usage: python karaoke.py file.smil"
-    raise SystemExit    
+    raise SystemExit
 try:
-    fich = open(fichero, 'r') 
+    fich = open(fichero, 'r')
     fich.close()
 except IOError:
     print "Introduce un nombre correcto de fichero.smil"
-    raise SystemExit  
+    raise SystemExit
 
 parser = make_parser()
 kHandler = SmallSMILHandler()
 parser.setContentHandler(kHandler)
 parser.parse(open(fichero))
-    
-for i in range(len(kHandler.lista)):
-    linea = kHandler.lista[i]
+
+for sublista in range(len(kHandler.lista)):
+    linea = kHandler.lista[sublista]
     tag = linea['name']
     imprimir = tag
     for atributo in kHandler.atributos[tag]:
+        if atributo == "src":
+            if linea[atributo][:7] == "http://":
+                recurso = linea[atributo]
+                os.system("wget -q " + recurso)
+                recurso_local = recurso.split('/')
+                linea[atributo] = recurso_local[-1]
         if not linea[atributo] == "":
-            imprimir = imprimir + "\\t" + atributo + '="' + linea[atributo] + '"'
+            imprimir = imprimir + "\\t" + atributo
+            imprimir = imprimir + '="' + linea[atributo] + '"'
     imprimir = imprimir + "\\n"
     print imprimir
-
